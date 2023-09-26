caaa6a76bb3d2d02bb205bb33133b04dad0aa6c6
Bitrey <alessandro.amella@live.it>
2023-09-26 20:15:39 +0200

    use node fs

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { Elysia } from "elysia";
 import { html } from "@elysiajs/html";
 import { staticPlugin } from "@elysiajs/static";
 import { cleanEnv, str, num } from "envalid";
+import { readFile } from "fs/promises";
 
 export const envs = cleanEnv(process.env, {
     NODE_ENV: str({
@@ -16,7 +17,7 @@ try {
     const app = new Elysia()
         .use(html())
         .use(staticPlugin())
-        .get("/", () => Bun.file("index.html").text())
+        .get("/", async () => await readFile("index.html", { encoding: "utf-8" }))
         .listen(envs.PORT);
     console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
 } catch (err) {
